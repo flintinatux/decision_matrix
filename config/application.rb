b5d0787681cb72acde71cae3d13c4019e8e15a5b
Scott McCormack <flintinatux@gmail.com>
2013-04-07 20:49:01 -0400

    Added concerns dirs

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module DecisionMatrix
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    # Custom directories with classes and modules you want to be eager-loaded.
+    config.eager_load_paths += %W(#{config.root}/app/models/concerns #{config.root}/app/controllers/concerns)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
